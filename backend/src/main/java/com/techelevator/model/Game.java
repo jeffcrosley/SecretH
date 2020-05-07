e7cbf330ad33667e453d95b8870a3d3b298556ff
Jeff Crosley <jeffcrosley@gmail.com>
2020-05-07 15:07:35 -0400

    created basic Game java object

--- backend/src/main/java/com/techelevator/model/Game.java
+++ backend/src/main/java/com/techelevator/model/Game.java
@@ -8,6 +8,45 @@ public class Game {
     private int fashPolicies;
     private String president;
     private String chancellor;
-
     
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumberOfPlayers() {
+		return numberOfPlayers;
+	}
+	public void setNumberOfPlayers(int numberOfPlayers) {
+		this.numberOfPlayers = numberOfPlayers;
+	}
+	public int getLibPolicies() {
+		return libPolicies;
+	}
+	public void setLibPolicies(int libPolicies) {
+		this.libPolicies = libPolicies;
+	}
+	public int getFashPolicies() {
+		return fashPolicies;
+	}
+	public void setFashPolicies(int fashPolicies) {
+		this.fashPolicies = fashPolicies;
+	}
+	public String getPresident() {
+		return president;
+	}
+	public void setPresident(String president) {
+		this.president = president;
+	}
+	public String getChancellor() {
+		return chancellor;
+	}
+	public void setChancellor(String chancellor) {
+		this.chancellor = chancellor;
+	}
+	
+	public Game() {
+
+	}    
 }
