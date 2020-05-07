950ff1ca5fc87b59922379e81b32e888bdcd2283
Jeff Crosley <jeffcrosley@gmail.com>
2020-05-07 11:46:43 -0400

    added admin role, logout button, and bound current user to app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,34 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
+      <a v-on:click="logOut">Log Out</a>
     </div>
-    <router-view/>
+    <router-view v-on:loggedIn="loggedIn"/>
   </div>
 </template>
+
+<script>
+import auth from './auth'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      currentUser: null
+    }
+  },
+  methods: {
+    loggedIn() {
+      this.currentUser = auth.getUser();
+    },
+    logOut() {
+      auth.logout();
+      this.$router.push('/login');
+    }
+  },
+  created() {
+    this.loggedIn();
+  }  
+}
+</script>
+
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p>Your name is {{currentUser.sub}}</p>
     <p>You must be authenticated to see this</p>
+    <p v-if="this.currentUser.rol == 'admin'">You must be an administrator to see this</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+      currentUser: this.$parent.currentUser
+    }
+  }
 }
 </script>
